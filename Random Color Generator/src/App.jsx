55a0db7e97b66ede5dcc7f807acdb3a00812ffd5
shanwaz-78 <mohammed.shanwaz78@gmail.com>
2024-04-01 01:17:43 +0530

    Made new projects using react

---
+++ Random Color Generator/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../src/App.css'
+import GenerateColor from './Components/ColorsContainer/GenerateColor'
+
+function App() {
+  return (
+    <>
+    <div className="App">
+      <GenerateColor />
+    </div>
+    </>
+  )
+}
+
+export default App
